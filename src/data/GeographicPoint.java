8fa59bcc424904e047125346f04179035ac3b7a5
caarlos-04 <cmazarico@gmail.com>
2024-12-15 12:22:33 +0100

    feat(gP): added method to calculate distance
    Co-authored-by: polma7 <polmasip7@gmail.com>
    Co-authored-by: caarlos-04 <cmazarico@gmail.com>

--- src/data/GeographicPoint.java
+++ src/data/GeographicPoint.java
@@ -40,4 +40,23 @@ final public class GeographicPoint {
         return "Geographic point {" + "latitude='" + latitude + '\'' +
                 "longitude='" + longitude + '}';
     }
+
+    public double CalculateDistance(GeographicPoint gP){
+        double lat1Rad = Math.toRadians(this.latitude);
+        double lon1Rad = Math.toRadians(this.longitude);
+        double lat2Rad = Math.toRadians(gP.latitude);
+        double lon2Rad = Math.toRadians(gP.longitude);
+
+        double deltaLat = lat2Rad - lat1Rad;
+        double deltaLon = lon2Rad - lon1Rad;
+
+        double a = Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2)
+                + Math.cos(lat1Rad) * Math.cos(lat2Rad)
+                * Math.sin(deltaLon / 2) * Math.sin(deltaLon / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+
+        return 6371 * c;
+
+    }
 }
